2ba07a439f3f4f657aaf0e3f68d4dc7563c79542
saef562012-boop <saef562012@gmail.com>
2026-04-23 23:20:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,15 @@
 const bedrock = require('bedrock-protocol');
 const express = require('express');
 const app = express();
-
-app.get('/', (req, res) => res.send('Bot is Working!'));
+app.get('/', (req, res) => res.send('Bot is Live'));
 app.listen(process.env.PORT || 3000);
 
-console.log("🔄 محاولة اقتحام السيرفر...");
+const client = bedrock.createClient({
+    host: "dynamic-8.magmanode.com",
+    port: 25753,
+    username: "Dura_Bot",
+    offline: true
+});
 
-function startBot() {
-    const client = bedrock.createClient({
-        host: "dynamic-8.magmanode.com",
-        port: 25753,
-        username: "Dura_AFK", // اسم جديد ومميز
-        offline: true,
-        version: "1.20.80" // سنستخدم هذا الإصدار كقناع للدخول
-    });
-
-    client.on('spawn', () => {
-        console.log("✅ أخيراً! البوت دخل السيرفر بنجاح.");
-    });
-
-    client.on('error', (err) => {
-        console.log("⚠️ محاولة اتصال جديدة...");
-    });
-
-    client.on('close', () => {
-        setTimeout(startBot, 5000);
-    });
-}
-
-startBot();
+client.on('spawn', () => console.log("✅ دخل البوت!"));
+client.on('error', (err) => console.log("❌ خطأ: " + err.message));
